c056595969ec7a88c086c574c6437d4fdbd87a18
Hanna Koh <hjk5643@g.rit.edu>
2024-02-24 17:19:07 -0500

    add alt text

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
+import { fetchAltText, uploadImg } from "./alt-text.js";
 
 addOnUISdk.ready.then(() => {
     console.log("addOnUISdk is ready for use.");
-
     const clickMeButton = document.getElementById("clickMe");
-    clickMeButton.addEventListener("click", () => {
+    clickMeButton.addEventListener("click", async () => {
         clickMeButton.innerHTML = "Clicked";
+        const selectedFile = document.getElementById("myFile").files[0];
+        const url = await uploadImg(selectedFile);
+        let data = await fetchAltText(url);
+        let text = data.captionResult
+        console.log(text);
+        displayText(text.text);
     });
 
     // Enable the button only when:
@@ -13,3 +19,8 @@ addOnUISdk.ready.then(() => {
     // 2. `click` event listener is registered.
     clickMeButton.disabled = false;
 });
+
+function displayText(text){
+    const txt = document.getElementById("alt-text").innerText = text;
+}
+
